268facb851b505419f756a7f59822b1537ff8bae
juancedeno96 <cedenojj7@gmail.com>
2021-05-20 11:56:58 -0600

    added Homepage route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import AuthPage from "./Pages/AuthPage";
 import CheckoutPage from "./Pages/CheckoutPage";
+import HomePage from "./Pages/HomePage";
 
 export default (
 <Switch>
   <Route exact path="/" component={AuthPage} />
   <Route path="/checkout" component={CheckoutPage} />
+  <Route path="/home" component={HomePage} />
 </Switch>
 );
