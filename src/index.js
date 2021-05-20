04edadf6c325b3d559d14b2a029d1fa54f39b736
juancedeno96 <cedenojj7@gmail.com>
2021-05-19 18:10:27 -0600

    added routes and redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import reportWebVitals from "./reportWebVitals";
+
+//Stripe
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe('pk_test_51IsBidK0LrulUWXA1zO0t8R70eZ0XYTeTFvirW8GxmXF6u2ID0f7Z5CIBAdiYjkF6sYCkWvinvjDBnAV1G69c1nd001zR2BuSC');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Elements stripe={stripePromise}>
+      <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+      </Provider>
+    </Elements>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
