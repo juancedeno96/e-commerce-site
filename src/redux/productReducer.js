07ff3cd330b4c3732363bc50682c8cde5e4783cb
juancedeno96 <cedenojj7@gmail.com>
2021-05-28 14:57:08 -0600

    product/cart reducer functionality

---
+++ src/redux/productReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    product: []
+  };
+  
+  const GET_PRODUCT = "GET_PRODUCT";
+  
+  export function getAllProduct(product) {
+    
+    return {
+      type: GET_PRODUCT,
+      payload: product,
+    };
+  }
+  
+  export default function productReducer(state = initialState, action) {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case GET_PRODUCT:
+        
+        return { ...state,  product: payload  };
+  
+      default:
+        return state;
+    }
+  }
+  
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import userReducer from './userReducer'
+import productReducer from './productReducer'
+import cartReducer from './cartReducer'
 import promiseMiddleware from 'redux-promise-middleware'
 
 
 const rootReducer = combineReducers({
     userReducer,
+    productReducer,
+    cartReducer
+    
     
 })
 
